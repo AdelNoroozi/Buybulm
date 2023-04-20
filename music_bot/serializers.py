a4d8bfa4fb87e839894e566daffe598709a85cd2
Adel <adel13no@gmail.com>
2023-04-20 23:01:12 +0430

    -music bot serializers file added -artist serializer added -artist mini serializer added

---
+++ music_bot/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from music_bot.models import *
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ('id', 'name', 'desc')
+        read_only_fields = ('id',)
+
+
+class ArtistMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ('id', 'name')
+        read_only_fields = ('id', 'name')
+
