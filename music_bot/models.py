88d8710c5b0d3697612d17b427faecb2d292114c
Adel <adel13no@gmail.com>
2023-04-20 21:51:45 +0430

    -music bot app added:  -music bot models added:   -Artist   -Album   -Song  -music bot installed in settings

---
+++ music_bot/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('artist name'))
+    desc = models.TextField(verbose_name=_('description'))
+
+    # photo = models.ImageField(blank=True, null=True,verbose_name='image',
+    #                           upload_to='images/',
+    #                           default='images/default.png')
+
+    class Meta:
+        verbose_name = _('Artist')
+        verbose_name_plural = _('Artists')
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('album title'))
+    desc = models.TextField(verbose_name=_('description'))
+    release_date = models.DateField(auto_now_add=True, verbose_name=_('release date'))
+    artists = models.ManyToManyField(Artist, verbose_name=_('artists'))
+    # cover = models.ImageField(blank=True, null=True,verbose_name='image',
+    #                           upload_to='images/',
+    #                           default='images/default.png')
+    min_price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name=_('minimum price'))
+
+    class Meta:
+        verbose_name = _('Album')
+        verbose_name_plural = _('Albums')
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, verbose_name=_('song title'))
+    desc = models.TextField(verbose_name=_('description'))
+    # cover = models.ImageField(blank=True, null=True,verbose_name='image',
+    #                           upload_to='images/',
+    #                           default='images/default.png')
+    artists = models.ManyToManyField(Artist, verbose_name=_('artists'))
+    album = models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE, verbose_name='album')
+    release_date = models.DateField(auto_now_add=True, verbose_name=_('release date'))
+    # file = models.FileField(verbose_name=_('file'))
+    lyrics = models.TextField(blank=True, null=True, verbose_name=_('lyrics'))
+    plays = models.PositiveIntegerField(default=0, verbose_name=_('plays'))
+
+    class Meta:
+        verbose_name = _('Song')
+        verbose_name_plural = _('Songs')
