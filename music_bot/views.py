f57449f01500776726f0db84038b43de500dd80f
Adel <adel13no@gmail.com>
2023-04-21 19:38:02 +0430

    -artist view set added -music bot app urls file added -music bot urls included in base urls -artist view set router registered in urls

--- music_bot/views.py
+++ music_bot/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from music_bot.models import *
+from music_bot.serializers import *
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ArtistMiniSerializer
+        else:
+            return ArtistSerializer
