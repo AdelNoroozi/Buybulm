a8e4942122183ce204029a478deaf8bb514acd6a
Adel <adel13no@gmail.com>
2023-04-27 16:24:01 +0430

    -search and ordering fields defined for artist, album and song view sets -pagination

--- buybulm/settings.py
+++ buybulm/settings.py
@@ -124,11 +124,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 SIMPLE_JWT = {
--- music_bot/views.py
+++ music_bot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
 from accounts.permissions import BotPermission
 from music_bot.models import *
 from music_bot.serializers import *
@@ -9,6 +11,9 @@ from music_bot.serializers import *
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     permission_classes = (BotPermission,)
+    filter_backends = [SearchFilter, ]
+    search_fields = ['name', 'desc']
+    pagination_class = PageNumberPagination
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -20,6 +25,10 @@ class ArtistViewSet(viewsets.ModelViewSet):
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     permission_classes = (BotPermission,)
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'desc', 'artists__name']
+    ordering_fields = ['release_date', 'min_price']
+    pagination_class = PageNumberPagination
 
     def get_serializer_class(self):
         if self.action == 'list':
@@ -31,6 +40,9 @@ class AlbumViewSet(viewsets.ModelViewSet):
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     permission_classes = (BotPermission,)
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['title', 'desc', 'artists__name', 'album__title', 'lyrics']
+    ordering_fields = ['release_date', 'plays']
 
     def get_serializer_class(self):
         if self.action == 'list':
