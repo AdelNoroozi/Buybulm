f8e9e9331e52f7a0b9dfc0bd41b1ac6ad74a355a
Adel <adel13no@gmail.com>
2023-04-27 20:50:45 +0430

    -payment serializer added -create payment view now returns a json of created payment instead of a message

---
+++ store/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from store.models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ('id', 'user', 'album', 'payment_time', 'price', 'receipt', 'message', 'user_preview_name', 'status')
+        read_only_fields = ('id', 'user', 'album', 'payment_time', 'price', 'receipt', 'message', 'user_preview_name')
